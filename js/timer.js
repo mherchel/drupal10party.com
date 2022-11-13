f3dc6a231a48b8a33b4f766d28ce74d6fa93ab5d
Mike Herchel <mike@herchel.com>
2022-11-13 14:01:44 -0500

    Countdown

---
+++ js/timer.js
@@ -0,0 +1,26 @@
+(() => {
+  function updateTimer() {
+    const future = Date.parse('2022-12-14T00:00:00Z');
+    const now = new Date();
+    const diff = future - now;
+
+    const secs = Math.floor(diff / 1000);
+    const mins = Math.floor(secs / 60);
+    const hours = Math.floor(mins / 60);
+    const days = Math.floor(hours / 24);
+
+    const s = secs - mins * 60;
+    const m = mins - hours * 60;
+    const h = hours - days * 24;
+    const d = days;
+
+    const countdown = document.querySelector('.countdown');
+
+    countdown.querySelector('[data-days').textContent = d;
+    countdown.querySelector('[data-hours').textContent = h;
+    countdown.querySelector('[data-minutes').textContent = m;
+    countdown.querySelector('[data-seconds').textContent = s;
+  }
+
+  setInterval(updateTimer, 1000);
+})();
