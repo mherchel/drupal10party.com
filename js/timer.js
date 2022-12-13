c646470654ce809a05c7042df95bea7331cecc85
Mike Herchel <mike@herchel.com>
2022-12-13 13:26:16 -0500

    Automate showing of zoom link

--- js/timer.js
+++ js/timer.js
@@ -4,23 +4,37 @@
     const now = new Date();
     const diff = future - now;
 
-    const secs = Math.floor(diff / 1000);
-    const mins = Math.floor(secs / 60);
-    const hours = Math.floor(mins / 60);
-    const days = Math.floor(hours / 24);
+    if (diff > 0) {
+      const secs = Math.floor(diff / 1000);
+      const mins = Math.floor(secs / 60);
+      const hours = Math.floor(mins / 60);
+      const days = Math.floor(hours / 24);
 
-    const s = secs - mins * 60;
-    const m = mins - hours * 60;
-    const h = hours - days * 24;
-    const d = days;
+      const s = secs - mins * 60;
+      const m = mins - hours * 60;
+      const h = hours - days * 24;
+      const d = days;
 
-    const countdown = document.querySelector('.countdown');
+      const countdown = document.querySelector('.countdown');
 
-    countdown.querySelector('[data-days').textContent = d;
-    countdown.querySelector('[data-hours').textContent = h;
-    countdown.querySelector('[data-minutes').textContent = m;
-    countdown.querySelector('[data-seconds').textContent = s;
+      countdown.querySelector('[data-days').textContent = d;
+      countdown.querySelector('[data-hours').textContent = h;
+      countdown.querySelector('[data-minutes').textContent = m;
+      countdown.querySelector('[data-seconds').textContent = s;
+    }
+    else {
+      document.querySelectorAll('[data-launch').forEach(el => {
+        el.style.display = "block";
+      });
+
+      document.querySelectorAll('[data-pre-launch]').forEach(el => {
+        el.style.display = "none";
+      });
+
+      clearInterval(refresh);
+    }
   }
 
-  setInterval(updateTimer, 1000);
+  updateTimer();
+  const refresh = setInterval(updateTimer, 1000);
 })();
